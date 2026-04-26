a3df5151380bd16abeff27d551caf689ce2300e6
ThanhLam2612 <luuthanhlam2612@gmail.com>
2026-04-26 22:57:56 +0700

    Xoa toan bo code tren GitHub

--- DoAnTotNghiep/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DoAnTotNghiep.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
